ba004aa900891a2c5b4d9d7e69ba15bb3b49fe8a
xx182pav <53460770+xx182pav@users.noreply.github.com>
2020-06-18 12:23:40 +0300

    Update settings.py

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vxx1yk6f-!8lsmbwq%uif^6(gxg6&n7d=ptkcg7_s**ctnciap'
+SECRET_KEY = os.getvenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
